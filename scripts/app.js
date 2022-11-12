93742aa3f3bfc73270ff67b0d56a287f6dba800d
rschoen <trschoen@gmail.com>
2022-11-11 17:38:36 -0800

    First pass at migration to new Google Identity services, though testing and cleanup still needed

--- scripts/app.js
+++ scripts/app.js
@@ -6,10 +6,85 @@
   app.displayInstalledToast = function() {
     document.querySelector('#caching-complete').show();
   };
+  
+  function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+  
+  function decodeJwtResponse (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+}
+
+	function signIn(id, token) {
+		document.getElementById("gamecontroller").setPlayer(id, token);
+	 document.querySelector('#signin').style.display = "none";
+	 document.querySelector('#signout').style.display = "block";
+	}
+	
+
+	function handleCredentialResponse(response) {
+     console.log("Encoded JWT ID token: " + response.credential);
+     const responsePayload = decodeJwtResponse(response.credential);
+
+     console.log("ID: " + responsePayload.sub);
+     console.log('Given Name: ' + responsePayload.given_name);
+	 setCookie("google_id",responsePayload.sub,6); // 7 days expiry baked into the server; expire before that
+	 setCookie("google_token",response.credential,6);
+	 signIn(responsePayload.sub,response.credential)
+	 
+		  /*  var profile = googleUser.getBasicProfile();
+  var id_token = googleUser.getAuthResponse().id_token;
+  document.getElementById("gamecontroller").setPlayer(profile.getId(), id_token);
+  
+  document.getElementById("gsignin").classList.add("hidden");
+  document.getElementById("signout").classList.remove("hidden");
+  console.log("Signed in as "+profile)*/
+        }
 
   app.addEventListener('dom-change', function() {
     console.log('Our app is ready to rock!');
-    appStart();
+	var google_id = getCookie("google_id");
+	var google_token = getCookie("google_token");
+	if(google_id && google_token) {
+		signIn(google_id,google_token);
+	}
+	
+	
+          google.accounts.id.initialize({
+            client_id: "168641906858-8egtsbds49ifcjgq7g6n4757q70k14h4.apps.googleusercontent.com",
+            callback: handleCredentialResponse
+          });
+          google.accounts.id.renderButton(
+            document.getElementById("signin"),
+            { theme: "outline", size: "large", text: "signin" }  // customization attributes
+          );
+	
+          //google.accounts.id.prompt(); // also display the One Tap dialog
+
   });
 
   window.addEventListener('WebComponentsReady', function() {
@@ -68,14 +143,16 @@
 })(document);
 
 function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
+/*  var profile = googleUser.getBasicProfile();
   var id_token = googleUser.getAuthResponse().id_token;
   document.getElementById("gamecontroller").setPlayer(profile.getId(), id_token);
   
   document.getElementById("gsignin").classList.add("hidden");
   document.getElementById("signout").classList.remove("hidden");
-  console.log("Signed in as "+profile)
+  console.log("Signed in as "+profile)*/
 }
+
+/*
 
 function signOut() {
   var auth2 = gapi.auth2.getAuthInstance();
@@ -91,10 +168,7 @@ var appStart = function() {
 };
 
 var initSigninV2 = function() {
-  auth2 = gapi.auth2.getAuthInstance(); /*init({
-      client_id: '168641906858-8egtsbds49ifcjgq7g6n4757q70k14h4.apps.googleusercontent.com',
-      scope: 'profile'
-  });*/
+  auth2 = gapi.auth2.getAuthInstance(); 
   
   document.getElementById('signin').appendChild(document.getElementById('gsignin'));
 
@@ -120,10 +194,10 @@ var userChanged = function (user) {
   googleUser = user;
 };
 
-/**
- * Retrieves the current user and signed in states from the GoogleAuth
- * object.
- */
+//**
+// * Retrieves the current user and signed in states from the GoogleAuth
+// * object.
+// 
 var refreshValues = function() {
   if (auth2){
     console.log('Refreshing values...');
@@ -132,4 +206,4 @@ var refreshValues = function() {
     onSignIn(googleUser);
     window.setTimeout(refreshValues, 1000 * 60 * 5);
   }
-}
+}*/
