4ce03085200b50b2c639c2ed4754dfe75bbe19d0
Ryan <trschoen@gmail.com>
2024-02-20 17:28:16 -0800

    Updated FCM messaging to latest version

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
-var VERSION = "1.1.29";
+var VERSION = "1.1.30";
 var CACHE_NAME = 'fireworks-cache-v' + VERSION;
 var urlsToCache = [
   'styles/app-theme.html'
@@ -82,26 +82,62 @@ self.addEventListener('activate', function(event) {
   );
 });
 
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
-importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-messaging.js');
 firebase.initializeApp({
-  'messagingSenderId': '168641906858'
+  apiKey: "AIzaSyAAR9ug59MJGzloy1_YiDZJDwebFSWcfv4",
+  authDomain: "fireworks-151422.firebaseapp.com",
+  databaseURL: "https://fireworks-151422.firebaseio.com",
+  projectId: "fireworks-151422",
+  storageBucket: "fireworks-151422.appspot.com",
+  messagingSenderId: "168641906858",
+  appId: "1:168641906858:web:5b7754f3db0783fc56e8d3",
+  measurementId: "G-4NPTP167DK"
 });
 
-
+// Initialize Firebase Cloud Messaging and get a reference to the service
 const messaging = firebase.messaging();
 
-messaging.setBackgroundMessageHandler(function(payload) {
-  console.log('Received background message ', payload);
+messaging.onBackgroundMessage((payload) => {
+  console.log('[serviceworker.js] Received background message ', payload);
   // Customize notification here
   const notificationTitle = "Fireworks - it's your turn!";
   const notificationOptions = {
-    body: 'Other players are waiting for you! Take your turn.',
-    icon: '/firebase-logo.png',
-    click_action: 'https://fellyeah.dlinkddns.com:80/#!/games/' + payload.data.Game
+    body: 'Tap here to take your turn.',
+    icon: 'https://ryanschoen.com/fireworks/images/icons/fireworks128.png',
+    data: 'https://www.ryanschoen.com/fireworks/#!/games/' + payload.data.game
   };
+  console.log('[serviceworker.js] Processed into notification ', notificationOptions);
 
-  return self.registration.showNotification(notificationTitle,
-      notificationOptions);
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
+
+self.addEventListener('notificationclick', function (event) {
+  console.log('On notification click: ', event.notification);
+  event.notification.close();
+  var redirectUrl = event.notification.data;
+  console.log("redirect url is : " + redirectUrl);
+
+  if (redirectUrl) {       
+      event.waitUntil(async function () {
+          var allClients = await clients.matchAll({
+              includeUncontrolled: true
+          });
+          var chatClient;            
+          for (var i = 0; i < allClients.length; i++) {
+              var client = allClients[i];                
+              if (client['url'].indexOf(redirectUrl) >= 0) {
+                  client.focus();
+                  chatClient = client;
+                  break;
+              }
+          }
+          if (chatClient == null || chatClient == 'undefined') {
+              chatClient = clients.openWindow(redirectUrl);
+              return chatClient;
+          }
+      }());        
+  }
 });
